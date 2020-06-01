e7ccdf557e0f2a7f70a4507c86d402217782ce4b
dfuyfkujdthb <66277257+dfuyfkujdthb@users.noreply.github.com>
2020-06-02 02:49:48 +0530

    Update infinite.py

--- infinite.py
+++ infinite.py
@@ -7,7 +7,7 @@ def telegram_bot_sendtext(bot_message):
     
     bot_token = '1193133723:AAEYOyi1qAOPNBsQMknpc6CnIXr33G7t4Ts'
     bot_chatID = '772671984'
-    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&text=' + bot_message
 
     response = requests.get(send_text)
 
@@ -16,12 +16,12 @@ def telegram_bot_sendtext(bot_message):
 
 def report():
     my_balance = 10   ## Replace this number with an API call to fetch your account balance
-    my_message = "Current balance is: {}".format(my_balance)   ## Customize your message
+    my_message = "https://www.acko.tech/amazon-mobile/download-policy/GuF71M6Cy5lhYT-Jek9law"   ## Customize your message
     telegram_bot_sendtext(my_message)
 
 
     
-schedule.every().day.at("02:28").do(report)
+schedule.every().day.at("02:50").do(report)
 
 while True:
     schedule.run_pending()
